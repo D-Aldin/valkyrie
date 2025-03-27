92736a57014e8005b0460e3c98a59ebe39d13bd4
D-Aldin <dobricaldin@gmail.com>
2025-03-27 22:12:04 +0100

    create background object

--- models/skeleton.class.js
+++ models/skeleton.class.js
@@ -2,5 +2,6 @@ class Skeleton extends MovableObject {
   constructor() {
     super();
     this.loadImage("./assets/image/skeleton_walking/0_Skeleton_Crusader_Walking_000.png");
+    this.x_position = 300 + Math.random() * 500;
   }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -1,6 +1,7 @@
 class World {
   character = new Valkyrie();
   enemies = [new Skeleton(), new Skeleton(), new Skeleton()];
+  canvas;
   ctx;
 
   constructor(canvas) {
@@ -9,7 +10,12 @@ class World {
   }
 
   draw() {
+    this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.canvas = canvas;
     this.ctx.drawImage(this.character.img, this.character.x_position, this.character.y_position, this.character.width, this.character.height);
+    this.enemies.forEach((skeleton) => {
+      this.ctx.drawImage(skeleton.img, skeleton.x_position, skeleton.y_position, skeleton.width, skeleton.height);
+    });
     let self = this;
     requestAnimationFrame(() => {
       self.draw();
