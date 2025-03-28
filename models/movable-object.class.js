42990f83c77a285369fd64ab6418c8878bc4908c
D-Aldin <dobricaldin@gmail.com>
2025-03-29 00:06:28 +0100

    create loadImagesToCache function to load all the img for the valkyrie character

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,10 +4,19 @@ class MovableObject {
   img;
   height = 180;
   width = 180;
+  imageCache = {};
 
   loadImage(path) {
     this.img = new Image();
     this.img.src = path;
+  }
+
+  loadImagesToCache(arr) {
+    arr.forEach((path) => {
+      let image = new Image();
+      image.src = path;
+      this.imageCache[path] = path;
+    });
   }
 
   moveRight() {
