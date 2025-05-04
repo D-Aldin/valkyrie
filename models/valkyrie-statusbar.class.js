8bcf41505d4a117d17b89cfc3e9c350f3a43da5a
D-Aldin <dobricaldin@gmail.com>
2025-05-04 22:53:16 +0200

    add jsdoc to valkyrie-statusbar class

--- models/valkyrie-statusbar.class.js
+++ models/valkyrie-statusbar.class.js
@@ -1,4 +1,14 @@
+/**
+ * Class representing the Valkyrie status bar.
+ * This class handles the display of the Valkyrie's health as an image, adjusting based on the health percentage.
+ *
+ * @extends DrawableObject
+ */
 class ValkyrieStatusBar extends DrawableObject {
+  /**
+   * Array of images representing the health status of the Valkyrie at different percentages.
+   * @type {string[]}
+   */
   healthValkyrie = [
     "./assets/image/status/health1.png",
     "./assets/image/status/health2.png",
@@ -8,9 +18,22 @@ class ValkyrieStatusBar extends DrawableObject {
     "./assets/image/status/health6.png",
   ];
 
+  /**
+   * The current health percentage of the Valkyrie.
+   * @type {number}
+   */
   percentageValkyrie = 100;
+
+  /**
+   * The amount of items collected by the Valkyrie (not currently used in the status bar, but initialized).
+   * @type {number}
+   */
   item = 0;
 
+  /**
+   * Creates an instance of the Valkyrie status bar.
+   * Initializes the health bar and sets the initial health percentage.
+   */
   constructor() {
     super();
     this.loadImage(this.healthValkyrie);
@@ -22,12 +45,23 @@ class ValkyrieStatusBar extends DrawableObject {
     this.height = 75;
   }
 
+  /**
+   * Sets the health percentage of the Valkyrie and updates the displayed image accordingly.
+   *
+   * @param {number} percentage - The health percentage to be set (between 0 and 100).
+   */
   setPercentage(percentage) {
     this.percentageValkyrie = percentage;
     let path = this.healthValkyrie[this.resolveImagePath(percentage) - 1];
     this.img = this.imageCache[path];
   }
 
+  /**
+   * Resolves the appropriate image index based on the current health percentage.
+   *
+   * @param {number} percentage - The current health percentage.
+   * @returns {number} The index of the health image corresponding to the given percentage.
+   */
   resolveImagePath(percentage) {
     if (percentage == 100) {
       return 6;
