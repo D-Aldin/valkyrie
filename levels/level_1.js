7b72a316c1154ff40a3e627c4d86ea0a82de8ae0
D-Aldin <dobricaldin@gmail.com>
2025-05-04 23:11:07 +0200

    remove the jsdoc from game.js because of a bug.

--- levels/level_1.js
+++ levels/level_1.js
@@ -1,32 +1,10 @@
-/**
- * Creates and returns the first level of the game.
- * This level includes various enemies, backgrounds, items, and gold to be used in the game.
- *
- * @returns {Level} The created Level object containing enemies, backgrounds, items, and gold.
- */
 function createLevel1() {
   return new Level(
+    [new Skeleton(), new Skeleton(), new Skeleton(), new Skeleton()],
+    [new Minotaur()],
+    [new Bats(), new Bats(), new Bats(), new Bats(), new Bats(), new Bats(), new Bats(), new Bats()],
     [
-      new Skeleton(),
-      new Skeleton(),
-      new Skeleton(),
-      new Skeleton(), // Array of Skeleton enemies
-    ],
-    [
-      new Minotaur(), // Array containing one Minotaur enemy
-    ],
-    [
-      new Bats(),
-      new Bats(),
-      new Bats(),
-      new Bats(),
-      new Bats(),
-      new Bats(),
-      new Bats(),
-      new Bats(), // Array of Bat enemies
-    ],
-    [
-      new Background("./assets/image/background/PNG/2/mirror5.png", -720), // Background objects for scrolling effect
+      new Background("./assets/image/background/PNG/2/mirror5.png", -720),
       new Background("./assets/image/background/PNG/2/5.png", 0),
       new Background("./assets/image/background/PNG/2/2.png", 0),
       new Background("./assets/image/background/PNG/2/mirror5.png", 720),
@@ -38,14 +16,7 @@ function createLevel1() {
       new Background("./assets/image/background/PNG/2/5.png", 2879),
       new Background("./assets/image/background/PNG/2/2.png", 2879),
     ],
-    [
-      new Gold(),
-      new Gold(),
-      new Gold(),
-      new Gold(),
-      new Gold(),
-      new Gold(), // Array of Gold objects for collection
-    ],
+    [new Gold(), new Gold(), new Gold(), new Gold(), new Gold(), new Gold()],
     [
       new Item(),
       new Item(),
@@ -58,7 +29,7 @@ function createLevel1() {
       new Item(),
       new Item(),
       new Item(),
-      new Item(), // Array of Item objects for collection
+      new Item(),
     ]
   );
 }
